2a7669f9a1fef34049b4686ee2f93d6d32330271
Raúl Alonso <rauldhjr07_07@hotmail.com>
2022-08-17 15:14:27 -0500

    Hola Mundo!

--- src/main/java/com/UdeaGrupo/Proyecto3/Proyecto3Application.java
+++ src/main/java/com/UdeaGrupo/Proyecto3/Proyecto3Application.java
@@ -2,12 +2,21 @@ package com.UdeaGrupo.Proyecto3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Proyecto3Application {
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola Ciclo3";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Proyecto3Application.class, args);
+
 	}
 
 }
